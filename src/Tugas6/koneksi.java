240fbeed7dcc87a1650f83b905d8df9d6d3f3955
ginofavianl <ginoleon97@gmail.com>
2017-04-06 18:49:42 +0800

    Tugas 6 Selesai

---
+++ src/Tugas6/koneksi.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas6;
+
+import com.mysql.jdbc.Connection;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author GinoFavianLeonardo
+ */
+public class koneksi {
+    private static Connection con;
+    
+    public static Connection getConnection(){
+        try{
+            con = (Connection) DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/pratikum_visual","root","");
+            JOptionPane.showMessageDialog(null, "Koneksi Berhasil");  
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null, "Koneksi Berhasil"
+            +e.getMessage());   
+        }
+        return con;
+    }
+    
+}
